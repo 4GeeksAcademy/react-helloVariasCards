236d86711c9f6cddbec6f0d1c98f456090bedfcb
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2024-01-05 16:03:31 +0000

    Finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,21 +5,46 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./nav.js";
 import Container from "./container.js";
 import Footer from "./footer.js";
+import Jumbotron from "./jumbotron.js";
 
 //create your first component
-   function Home() {
+function Home() {
 	return (
 		<>
-	<Navbar	/>
-	<Container />
-	<Footer/>
-	</>
-	);
+			<div className="text-center">
+				<Navbar />
+				<div>
+					<Jumbotron />
+					</div >
+					<div className="container">
+						<div className="row p-2 g-5">
+							<div class="col-sm-3 col-md-3 ">
+								<Container title="Picture1" image="https://viajes.nationalgeographic.com.es/medio/2017/04/24/bali-indonesia_5ae3dbfe.jpg" />
+								</div>
+							<div className="col-sm-3  col-md-3 ">
+								<Container title="Picture1" image="https://viajes.nationalgeographic.com.es/medio/2017/04/24/bali-indonesia_5ae3dbfe.jpg" />
+							</div>
+						<div className="col-sm-3 col-md-3 ">
+							<Container title="Picture1" image="https://viajes.nationalgeographic.com.es/medio/2017/04/24/bali-indonesia_5ae3dbfe.jpg" />
+						</div>
+						<div className="col-sm-3 col-md-3 ">
+							<Container title="Picture1" image="https://viajes.nationalgeographic.com.es/medio/2017/04/24/bali-indonesia_5ae3dbfe.jpg" />
+						</div>
+					</div>
+
+					</div>
+				
+			</div >
+		
+			<Footer />
+			</>
+
+			);
 
 };
 
 
 
-export default Home; 
+export default Home;
 
 
---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Jumbotron = ()=>{
+    return (
+        <div className= "row d-flex justify-content-center">
+            <div className= "col-11">
+        <div class="jumbotron m-5 bg-secondary-subtle">
+  <h1 class="display-4 ms-3">A Warm Welcome!</h1>
+  <p class="lead ms-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <a class="btn btn-primary btn-lg mb-3 ms-3" href="#" role="button">Call to action</a>
+</div>
+</div>
+</div>
+    );
+};
+
+export default Jumbotron;
